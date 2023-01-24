7a99b58f170d51250a30b05148c4e4cb7534fda9
patrick d miller <patrickdmiller@gmail.com>
2023-01-24 12:36:46 -0500

    updates

--- src/config/user-defined/constants.js
+++ src/config/user-defined/constants.js
@@ -2,6 +2,9 @@ const APP_IDS = {
   BLOCKSQUISH: "blocksquish",
   BALLPIT: "ballpit",
   SQUISHY: "squishy",
+  AUTOMATA: "automata",
+  BOUNCE:"bouncetype",
+  HYPERSPACE:'hyperspace'
 };
 
 module.exports = {
